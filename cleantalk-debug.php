d1f370d94632d5b6eb2a0105cc3fa4c8def010fc
Glomberg <bazz@bk.ru>
2022-03-03 15:35:11 +0300

    Version 2.0

--- cleantalk-debug.php
+++ cleantalk-debug.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Cleantalk Debug
  * Author:            CleanTalk team
- * Version:           1.0
+ * Version:           2.0
  */
 
 // If this file is called directly, abort.
